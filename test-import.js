f80361db18b8e3c27e943fcf81e2a4515f73c98b
matilefaco <43683093+matilefaco@users.noreply.github.com>
2026-05-13 09:37:03 -0300

    chore: remove orphaned test and patch scripts

--- test-import.js
+++
@@ -1,1 +0,0 @@
-import('./dist-server/server/api_entry.js').then(m => console.log('Import successful')).catch(e => console.error('Import failed', e));
--- test-req.js
+++
@@ -1,7 +0,0 @@
-import('./dist-server/server.js').then(async m => {
-  console.log('Got server module');
-  const app = await m.createServerApp();
-  console.log('App created');
-}).catch(e => {
-  console.error('Fatal:', e);
-});
